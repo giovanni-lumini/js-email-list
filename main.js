4f58ecd3f92d37e0e0cabf99f6d3eaa7b402cb87
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-17 15:00:08 +0200

    link_api

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 /* 
 Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
+
+//accedere all'API
+//prendere solo l'email randomica
+//inserirla in un li
+//generare 10 li
+//inserirli nel ul del DOM
+
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+.then(response => {
+    //accesso a tutti i dati
+    const result_response = response;
+    console.log(result_response);
+    //accesso a tutti i dati, nello specifico a data
+    const result_response_data = response.data;
+    console.log(result_response_data);
+    //accesso a tutti i dati, nello specifico a response, all'interno di data (solo l'email randomica)
+    const result_response_data_response = response.data.response;
+    console.log(result_response_data_response);
+})
